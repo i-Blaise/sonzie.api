baeda1793f32b5bbab86c876cacb6a19b06df8d3
Blaise <menniablaise@yahoo.com>
2020-10-15 15:50:39 +0000

    db

--- config/database.php
+++ config/database.php
@@ -11,7 +11,7 @@ return array(
             'host'      => '127.0.0.1',
             'database'  => 'sonzie',
             'username'  => 'root',
-            'password'  => '',
+            'password'  => 'r00t@Hy!ass',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
